a026eb598b43ec55c57850acc39de8519a957a0c
flipmz <zorigmr@gmail.com>
2020-04-27 04:16:36 +0800

    nested

--- nested1/src/App.js
+++ nested1/src/App.js
@@ -11,20 +11,35 @@ function App() {
         bg="dark"
         variant="dark"
       >
-        <ReactBootStrap.Navbar.Brand href="#home">
-          React-Bootstrap
-        </ReactBootStrap.Navbar.Brand>
         <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
           <ReactBootStrap.Nav className="mr-auto">
             <ReactBootStrap.Nav.Link href="#features">
-              Features
+              ТАНИЛЦУУЛГА
             </ReactBootStrap.Nav.Link>
             <ReactBootStrap.Nav.Link href="#pricing">
-              Pricing
+              ХУУЛЬ ЭРХ ЗҮЙ
             </ReactBootStrap.Nav.Link>
             <ReactBootStrap.NavDropdown
-              title="Dropdown"
+              title="ХУУЛЬ ЭРХ ЗҮЙ"
+              id="collasible-nav-dropdown"
+            >
+              <ReactBootStrap.NavDropdown.Item href="#action/3.1">
+                Action
+              </ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Item href="#action/3.2">
+                Another action
+              </ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Item href="#action/3.3">
+                Something
+              </ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Divider />
+              <ReactBootStrap.NavDropdown.Item href="#action/3.4">
+                Separated link
+              </ReactBootStrap.NavDropdown.Item>
+            </ReactBootStrap.NavDropdown>
+            <ReactBootStrap.NavDropdown
+              title="МЭДЭЭ МЭДЭЭЛЭЛ"
               id="collasible-nav-dropdown"
             >
               <ReactBootStrap.NavDropdown.Item href="#action/3.1">
